5ef59de0f123e63d14b026f7d5e76818c55025b0
YubiFuu <jess.supercode@gmail.com>
2022-12-06 15:24:54 +0100

    css styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,22 @@
 import "./Nav.css";
+import { Link } from "react-router-dom";
+import pokemon from "../img/pokemon.png";
+import Burger from "../img/Burger.png";
+import DarkMode from "../img/DarkMode.png";
 const Nav = () => {
-    return <header></header>;
+    console.log(pokemon);
+    return (
+        <header>
+            <Link to={"/"}>
+                <img src={pokemon} alt="pokemon" />
+            </Link>
+            <div>
+                <img src={Burger} alt="Burger icon" />
+                <input type="text" placeholder="Search Pokemon" />
+                <img src={DarkMode} alt="Darkmode icon" />
+            </div>
+        </header>
+    );
 };
 
 export default Nav;
