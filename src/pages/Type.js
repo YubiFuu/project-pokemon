3d3b99f568353cbab9b6be828b52326d19eaf1ec
“NilsNeumann11” <supercoder@SuperCoders-Air.fritz.box>
2022-12-06 15:34:31 +0100

    button list html

--- src/pages/Type.js
+++ src/pages/Type.js
@@ -1,6 +1,31 @@
 import "./Type.css";
 const Type = () => {
-    return <main></main>;
+  return (
+    <main>
+      <h2>Type</h2>
+      <section className="type-grid-container">
+        <button className="type-grid-item color1">Bug</button>
+        <button className="type-grid-item color2">Dark</button>
+        <button className="type-grid-item color3">Dragon</button>
+        <button className="type-grid-item color4">Electric</button>
+        <button className="type-grid-item color5">Fairy</button>
+        <button className="type-grid-item color6">Fighting</button>
+        <button className="type-grid-item color7">Fire</button>
+        <button className="type-grid-item color8">Flying</button>
+        <button className="type-grid-item color9">Ghost</button>
+        <button className="type-grid-item color10">Grass</button>
+        <button className="type-grid-item color11">Ground</button>
+        <button className="type-grid-item color12">Ice</button>
+        <button className="type-grid-item color13">Normal</button>
+        <button className="type-grid-item color14">Plant</button>
+        <button className="type-grid-item color15">Poison</button>
+        <button className="type-grid-item color16">Psychic</button>
+        <button className="type-grid-item color17">Rock</button>
+        <button className="type-grid-item color18">Steel</button>
+        <button className="type-grid-item color19">Water</button>
+      </section>
+    </main>
+  );
 };
 
 export default Type;
